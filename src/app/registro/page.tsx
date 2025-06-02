b757d6da9729bf7e0024f2fa7676bc4fcdcc2e58
Rodas <68786750+Rodas033@users.noreply.github.com>
2025-06-02 12:09:58 -0500

    Crear página de inicio de sesión

---
+++ src/app/registro/page.tsx
@@ -0,0 +1,37 @@
+import { LayoutDashboard } from "lucide-react"
+import { SignUpForm } from "@/components/signup-form"
+import Link from "next/link";
+
+export default function Registro() {
+  return (
+    <div className="grid min-h-svh lg:grid-cols-2">
+      <div className="bg-muted relative hidden lg:block">
+        <img
+          src="/images/placeholder.jpg"
+          alt="Imagen lateral"
+          className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+        />
+      </div>
+      <div className="flex flex-col gap-4 p-6 md:p-10">
+        <div className="flex flex-col justify-center gap-2 md:justify-start">
+          <img
+              src="/images/logo.png"
+              alt="Imagen de logo"
+              className="max-w-[164px]"
+            />
+          <Link href="#" className="flex items-center gap-2 font-medium">
+            <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
+              <LayoutDashboard className="size-4" />
+            </div>
+            CRM Cooprudea
+          </Link>
+        </div>
+        <div className="flex flex-1 items-center justify-center">
+          <div className="w-full max-w-xs">
+            <SignUpForm />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
